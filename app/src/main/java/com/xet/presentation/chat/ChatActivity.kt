6a505bb8152b01933052f74af6d5c1ec9685efb1
Mateus Lucas <mateuxlucax@gmail.com>
2022-10-18 18:08:09 -0300

    feat: make possible to record audio (needed to bump min sdk to 31)

--- app/src/main/java/com/xet/presentation/chat/ChatActivity.kt
+++ app/src/main/java/com/xet/presentation/chat/ChatActivity.kt
@@ -1,10 +1,20 @@
 package com.xet.presentation.chat
 
+import android.Manifest.permission.ACCESS_MEDIA_LOCATION
+import android.Manifest.permission.RECORD_AUDIO
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import com.google.android.material.color.MaterialColors
 import com.xet.databinding.ActivityChatBinding
@@ -12,6 +22,10 @@ import com.xet.domain.model.Message
 import com.xet.domain.model.User
 import com.xet.presentation.ServiceLocator
 import com.xet.presentation.chat.components.ChatAdapter
+import java.io.IOException
+
+
+const val REQUEST_AUDIO_PERMISSION_CODE = 1
 
 class ChatActivity(
     private val viewModel: ChatViewModel = ServiceLocator.getChatViewModel()
@@ -20,6 +34,9 @@ class ChatActivity(
     private lateinit var binding: ActivityChatBinding
     private lateinit var friend: User
     private val messages: MutableList<Message> = mutableListOf()
+    private var recorder: MediaRecorder? = null
+    private var isRecording = false
+    private var audioFile: String? = null
 
     @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,13 +82,22 @@ class ChatActivity(
         loading.visibility = View.VISIBLE
         viewModel.loadMessages()
 
+        chatButton.setOnClickListener {
+            if (isRecording) {
+                stopAudioRecording()
+                playAudio()
+            } else {
+                startAudioRecording()
+            }
+        }
+
         val color = MaterialColors.getColor(this, com.google.android.material.R.attr.colorSecondaryVariant, Color.BLACK)
         window.navigationBarColor = color
     }
 
     private fun loadExtras() {
         val bundle = intent.extras
-        val id = bundle?.getString(Companion.FRIEND_ID)
+        val id = bundle?.getString(FRIEND_ID)
         val name = bundle?.getString(FRIEND_NAME)
         val username = bundle?.getString(FRIEND_USER_NAME)
 
@@ -84,6 +110,81 @@ class ChatActivity(
         const val FRIEND_ID = "friend_id"
         const val FRIEND_NAME = "friend_name"
         const val FRIEND_USER_NAME = "friend_user_name"
+    }
+
+    private fun isAudioPermissionGranted(): Boolean {
+        val storagePermission = ContextCompat.checkSelfPermission(applicationContext, ACCESS_MEDIA_LOCATION)
+        val audioPermission = ContextCompat.checkSelfPermission(applicationContext, RECORD_AUDIO)
+        return storagePermission == PackageManager.PERMISSION_GRANTED && audioPermission == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this@ChatActivity,
+            arrayOf(RECORD_AUDIO, ACCESS_MEDIA_LOCATION),
+            REQUEST_AUDIO_PERMISSION_CODE,
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_AUDIO_PERMISSION_CODE
+            && grantResults.first() == PackageManager.PERMISSION_GRANTED
+            && grantResults.last() == PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: replace with string resource
+            Toast.makeText(applicationContext, "Permission Granted", Toast.LENGTH_LONG).show()
+        } else {
+            // TODO: replace with string resource
+            Toast.makeText(applicationContext, "Permission Denied", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun startAudioRecording() {
+        if (isAudioPermissionGranted()) {
+            recorder = MediaRecorder(this)
+
+            audioFile = "${Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) }/audio.3gp    "
+            recorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
+            recorder?.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            recorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            recorder?.setOutputFile(audioFile)
+
+            try {
+                recorder?.prepare()
+            } catch (e: Exception) {
+                Log.w("error while recording audio.", e)
+            }
+
+            recorder?.start()
+            isRecording = true
+            Log.i("event", "start_recording")
+        } else {
+            requestPermissions()
+        }
+    }
+
+    private fun stopAudioRecording() {
+        recorder?.stop()
+        recorder?.release()
+        isRecording = false
+
+        Log.i("event", "stop_recording")
+    }
+
+    private fun playAudio() {
+        val player = MediaPlayer()
+        try {
+            player.setDataSource(audioFile)
+            player.prepare()
+            player.start()
+        } catch (e: IOException) {
+            Log.e("TAG", "prepare() failed")
+        }
     }
 
 }
